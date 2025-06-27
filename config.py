b1b7d1ed9bd8a86a560fac308ae4a6448e27e9f3
meteora9538 <153423635+meteora9538@users.noreply.github.com>
2025-06-27 21:22:46 +0800

    config.py

--- config.py
+++ config.py
@@ -5,17 +5,17 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 # Authentication for user filing issue (must have read/write access to repository to add issue to)
-USERNAME = 'olozhika'
+USERNAME = 'meteora9538'
 
 # The repository to add this issue to
-REPO_OWNER = 'olozhika'
-REPO_NAME = 'ArXivDaily_StarFormation'
+REPO_OWNER = 'meteora9538'
+REPO_NAME = 'ArXivDaily_AngularMomentum'
 
 # Set new submission url of subject
 NEW_SUB_URL = 'https://arxiv.org/list/astro-ph/new'
 
 # Keywords to search
-KEYWORD_LIST = ["star formation", "star-forming", "molecular cloud", "interstellar medium", "cloud", "clump", "core", "filament", "atomic gas", "N-PDF"]
+KEYWORD_LIST = ["star formation", "star-forming", "angular momentum", "spin", "disk galaxy", "fast rotator", "IMF", "environment", "brightest cluster galaxies", "H alpha emitter"]
 # Keywords to exclude
-KEYWORD_EX_LIST = ["galaxies", "galaxy cluster", " AGN ", "standard candle", "X-ray binar", "solar corona"]
+KEYWORD_EX_LIST = ["gravitational wave", "dark energy"]
 # Note that the 'Keywords' above are actually searched in the abstract instead of the real keyword section. 
